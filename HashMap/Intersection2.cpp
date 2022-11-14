5c7403f6c4f8215143391cc3528d7a6da800d61f
kanhaiya <kanhaiyarmy@gmail.com>
2022-11-13 22:53:32 -0600

    Find the common element between 2 arrays with their min freq

---
+++ HashMap/Intersection2.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+    unordered_map<int,int> freq;
+    for(int n : nums1){
+        freq[n]++;
+    }
+    
+    vector<int> res;
+    for(int n : nums2){
+        if(freq.find(n) != freq.end()){
+            res.push_back(n);
+            freq.erase(n);
+        }
+    }
+    
+    return res;
+}
